f51e0bf184ba11762302eb3c79cf616a9df05bb5
colby <fornerette.colby@gmail.com>
2025-12-14 13:09:55 -0700

    added react frontsend skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Edge Lab</h1>
+      <p>Frontend running</p>
+    </div>
+  );
+}
+
+export default App;
+
