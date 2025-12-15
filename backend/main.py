aa2e5e72743958600295cdb566d809a9a2ff2ab9
colby <fornerette.colby@gmail.com>
2025-12-15 11:08:47 -0700

    Open CORS blockage

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = FastAPI(title="Edge Lab API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
