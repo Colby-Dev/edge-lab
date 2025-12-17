f0b0bf6fbc149e7a5d97e5ce47433922e21408b2
colby <fornerette.colby@gmail.com>
2025-12-17 00:30:32 -0700

    updated sql string to original PG

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://KwV%y?.b_weC8&U@db.mxvjxkrhuvsckftmnizs.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres:KwV%y?.b_weC8&U@db.mxvjxkrhuvsckftmnizs.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
