c8bfe6de64b2c5571804499840e6ec8b5b833fd2
colby <fornerette.colby@gmail.com>
2025-12-17 00:39:53 -0700

    added os import to db.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://postgres:KwV%y?.b_weC8&U@db.mxvjxkrhuvsckftmnizs.supabase.co:5432/postgres"
 
+DATABASE_URL = os.environ["DATABASE_URL"]
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
