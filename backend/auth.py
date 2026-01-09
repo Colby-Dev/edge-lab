b7fbac09701d3ba338d3ce07c681fcea69ed431b
colby <fornerette.colby@gmail.com>
2026-01-09 11:14:34 -0700

    payload optimization

--- backend/auth.py
+++ backend/auth.py
@@ -22,7 +22,7 @@ def get_current_user(authorization: str = Header(None)):
            options={"verify_aud": False},
         #    audience="authenticated",
        )
-       return {"user_id": payload.get("sub")}
+       return payload#{"user_id": payload.get("sub")}
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
